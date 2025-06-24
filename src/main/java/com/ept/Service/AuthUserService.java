5296868ffca8d208ef734632301e969869b3f305
RegithubDev <146913864+RegithubDev@users.noreply.github.com>
2025-06-24 12:22:06 +0530

    Update AuthUserService.java

--- src/main/java/com/ept/Service/AuthUserService.java
+++ src/main/java/com/ept/Service/AuthUserService.java
@@ -1,22 +1,3 @@
-//package com.ept.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import com.ept.Entity.Employees;
-//import com.ept.Entity.LoginResponse;
-//import com.ept.Entity.User;
-//
-//
-//public interface AuthUserService {
-//	
-//	String signup(User user);	
-//	Optional<LoginResponse> login(String email, String role,String password);	
-//	User getUserByEmail(String emali);
-//	List<Employees> getAllemployees();
-//	
-//
-//}
 
 package com.ept.Service;
 
