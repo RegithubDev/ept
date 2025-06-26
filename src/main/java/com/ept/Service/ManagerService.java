b584280c07a881aa436e7c1583b3fc94b07e25f1
RegithubDev <146913864+RegithubDev@users.noreply.github.com>
2025-06-26 13:54:41 +0530

    Update ManagerService.java

--- src/main/java/com/ept/Service/ManagerService.java
+++ src/main/java/com/ept/Service/ManagerService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import com.ept.Entity.Task;
 import com.ept.Entity.User;
 
-
 public interface ManagerService {
 	
 	String taskasign(Task task);
@@ -18,4 +17,6 @@ public interface ManagerService {
 	String update(Long id ,Task task);
 	
 	String delById(Long id);
+
+	String delEmpByMail(String email);
 }
